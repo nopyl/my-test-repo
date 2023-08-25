a71b2965ee0e78bf00d258488235f11ea7d7f0ea
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-08-25 16:13:13 +0300

    Create sendPhoneVerificationCode

--- api/services/sms/sms.service.js
+++ api/services/sms/sms.service.js
@@ -1,5 +1,8 @@
 import AWS from "aws-sdk";
 import dotenv from "dotenv";
+import { generateRandomInt } from "../../utils/helpers/token.helper.js";
+import { SmsOption } from "./SmsOption.js";
+import { SmsInfo } from "./SmsInfo.js";
 
 dotenv.config({ path: "./config/config.env" });
 
@@ -13,12 +16,37 @@ AWS.config.update({
     }
 });
 
-export const sendSms = (smsOption) => {
+const sendSms = (smsOption) => {
 
     return new AWS.SNS()
     .publish(smsOption)
     .promise()
     .then()
     .catch(err => console.log(err));
+
+}
+
+export const sendPhoneVerificationCode = async(user, next) => {
+
+    const {PHONE_VERIFICATION_CODE_EXPIRES} = process.env;
+    
+    const otp = generateRandomInt(6);
+
+    try{
+        user.phoneNumberCode = otp;
+        user.phoneNumberCodeExpires = new Date(Date.now() + Number(PHONE_VERIFICATION_CODE_EXPIRES));
+    
+        await user.save();
+    }
+    catch(err){
+        return next(err);
+    }
+
+    const option = new SmsOption(
+        user.phoneNumber,
+        SmsInfo.phoneVerificationMessage(otp)
+    );
+
+    sendSms(option);
 
 }
