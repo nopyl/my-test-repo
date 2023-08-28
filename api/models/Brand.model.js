0a9cc1cfe6902cebf29fce1d7d6bcfdce3812a1b
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-08-28 18:40:27 +0300

    Create Brand model

---
+++ api/models/Brand.model.js
@@ -0,0 +1,32 @@
+import { DataTypes } from "sequelize";
+import db from "../services/database/service.database.js";
+
+const Brand = db.define("Brand", {
+
+    uuid: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4
+    },
+    brandName: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false
+    },
+    userId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+    },
+    isVisible: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW   
+    }
+
+}, {timestamps: false});
+
+await Brand.sync();
+export default Brand;
