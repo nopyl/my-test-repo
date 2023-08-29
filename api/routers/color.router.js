50fd19e3ddf57acb3a21ec540c03bcb3387ce740
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-08-29 12:23:40 +0300

    Add getStaffAccess middleware to color router

--- api/routers/color.router.js
+++ api/routers/color.router.js
@@ -2,10 +2,11 @@ import { Router } from "express";
 import passport from "passport";
 import { createColor, updateColor, deleteColor, getColorById, getAllColors } from "../controllers/color.controller.js";
 import { checkColorExists } from "../middlewares/query/query.middleware.js";
+import { getStaffAccess } from "../middlewares/auth/auth.middleware.js";
 
 export const colorRouter = Router();
 
-colorRouter.use(passport.authenticate("jwt", {session: false}));
+colorRouter.use([passport.authenticate("jwt", {session: false}), getStaffAccess]);
 
 colorRouter.post("/create", createColor);
 colorRouter.patch("/update/:uuid", checkColorExists, updateColor)
